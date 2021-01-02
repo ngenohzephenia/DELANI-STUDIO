c92aaa380db13174fcdb46cc4fc366c855a1d0b2
Moringa Access <ngenohzef@gmail.com>
2021-01-02 11:50:03 +0300

    README.md

---
+++ js/script.js
@@ -0,0 +1,118 @@
+$('.main').hover(function(){
+  $(".main").hover(function () {
+    $(this).children(".second").show();
+    $(".img8").css({
+      opacity:0.6
+  });
+  });
+  $(".main").mouseleave(function(){
+          $(".img8").css({
+            opacity:0.9
+        });
+        $(".second").hide();
+  });
+});
+$(document).ready(function() {
+  $(".click1").click(function() {
+    $(".hide1").slideToggle();
+  });
+  $(".click2").click(function() {
+      $(".hide2").slideToggle();
+});
+$(".click3").click(function() {
+  $(".hide3").slideToggle();
+});
+
+  // $(".work1").mouseover(function(){
+  //   $(".work1a").show();
+
+  // }).mouseout(function(){
+  //   $(".work1a").hide();
+  // })
+
+  $(function(){
+    $(".work1").mouseover(function(){
+      $(".work1a").show();
+    }).mouseleave(function() {
+      $(".work1a").hide();
+    })
+  })
+
+  $(function(){
+    $(".work2").mouseover(function(){
+      $(".work2a").show();
+    }).mouseleave(function() {
+      $(".work2a").hide();
+    })
+  })
+
+  $(function(){
+    $(".work3").mouseover(function(){
+      $(".work3a").show();
+    }).mouseleave(function() {
+      $(".work3a").hide();
+    })
+  })
+
+  $(function(){
+    $(".work4").mouseover(function(){
+      $(".work4a").show();
+    }).mouseleave(function() {
+      $(".work4a").hide();
+    })
+  })
+
+  $(function(){
+    $(".work5").mouseover(function(){
+      $(".work5a").show();
+    }).mouseleave(function() {
+      $(".work5a").hide();
+    })
+  })
+
+  $(function(){
+    $(".work6").mouseover(function(){
+      $(".work6a").show();
+    }).mouseleave(function() {
+      $(".work6a").hide();
+    })
+  })
+
+  $(function(){
+    $(".work7").mouseover(function(){
+      $(".work7a").show();
+    }).mouseleave(function() {
+      $(".work7a").hide();
+    })
+  })
+
+  $(function(){
+    $(".work8").mouseover(function(){
+      $(".work8a").show();
+    }).mouseleave(function() {
+      $(".work8a").hide();
+    })
+  })
+
+
+  $("form#contact-form").on("submit", function(event) {
+    event.preventDefault();
+    var name = $("input#form_name").val();
+    var enail= $("input#form_email").val();
+    var message = $("input#form_message").val();
+    var result = ("Hello " + name + " we received your message. Thank you for reaching to us well get back soon");
+
+    $("#output").text(result);
+
+    // if($("input#form_name").val() && $("input#form_email").val() && $("input#form_message").val() ){
+    //   alert("Hello" + name + "Thanks for reaching, we'll reach out to you soon! ");
+    // }
+    // else if ($("input#form_name").val() && $("input#form_email").val() ){
+    //   alert("Kindly leave a message");
+    // }
+
+  });
+
+
+
+  });
